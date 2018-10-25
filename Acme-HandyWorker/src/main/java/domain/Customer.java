1047bd5774076ee5136d43ce59b7c7c0e838d1d9
Ferkaj <f_javi_fm@hotmail.com>
2018-10-25 16:38:07 +0200

    Problema 6

--- Acme-HandyWorker/src/main/java/domain/Customer.java
+++ Acme-HandyWorker/src/main/java/domain/Customer.java
@@ -1,18 +1,36 @@
 
 package domain;
 
+import java.util.Collection;
+
+import javax.persistence.OneToMany;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class Customer extends Actor {
 
-	//	private Collection<Endorser>	myEndorsers;
-	//
-	//
-	//	@NotEmpty
-	//	@OneToMany()
-	//	public Collection<Endorser> getMyEndorsers() {
-	//		return this.myEndorsers;
-	//	}
-	//	
-	//	public void setMyEndorsers(E)
+	private Collection<Endorser>	myEndorsees;
+	private Collection<Endorser>	myEndorsers;
+
+
+	@NotEmpty
+	@OneToMany()
+	public Collection<Endorser> getmyEndorsees() {
+		return this.myEndorsees;
+	}
+
+	public void setMyEndorsers(final Collection<Endorser> endorsers) {
+		this.myEndorsees = endorsers;
+	}
+
+	@NotEmpty
+	@OneToMany()
+	public Collection<Endorser> getmyEndorsers() {
+		return this.myEndorsers;
+	}
+
+	public void setmyEndorsers(final Collection<Endorser> endorsers) {
+		this.myEndorsers = endorsers;
+	}
 
 }
--- Acme-HandyWorker/src/main/java/domain/Handyworker.java
+++ Acme-HandyWorker/src/main/java/domain/Handyworker.java
@@ -1,6 +1,36 @@
 
 package domain;
 
+import java.util.Collection;
+
+import javax.persistence.OneToMany;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
 public class Handyworker extends Actor {
+
+	private Collection<Endorser>	myEndorsees;
+	private Collection<Endorser>	myEndorsers;
+
+
+	@NotEmpty
+	@OneToMany()
+	public Collection<Endorser> getmyEndorsees() {
+		return this.myEndorsees;
+	}
+
+	public void setMyEndorsers(final Collection<Endorser> endorsers) {
+		this.myEndorsees = endorsers;
+	}
+
+	@NotEmpty
+	@OneToMany()
+	public Collection<Endorser> getmyEndorsers() {
+		return this.myEndorsers;
+	}
+
+	public void setmyEndorsers(final Collection<Endorser> endorsers) {
+		this.myEndorsers = endorsers;
+	}
 
 }
