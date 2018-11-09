e262317b2d9318ba268b06a451046cf81eec93a5
Julian Lopez <julianlj94@hotmail.com>
2018-11-09 20:53:59 +0100

    Modificado Dominio y Populate

--- Acme-HandyWorker/src/main/java/domain/Customer.java
+++ Acme-HandyWorker/src/main/java/domain/Customer.java
@@ -1,9 +1,14 @@
 
 package domain;
 
+import java.util.Collection;
+
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -13,5 +18,19 @@ public class Customer extends Endorser {
 	// ATRIBUTOS
 
 	// Relationships ---------------------------------------------------------
+
+	private Collection<FixUpTask> fixUpTasks;
+
+
+	@NotNull
+	@Valid
+	@OneToMany(mappedBy = "customer")
+	public Collection<FixUpTask> getFixUpTasks() {
+		return this.fixUpTasks;
+	}
+
+	public void setFixUpTasks(final Collection<FixUpTask> fixUpTasks) {
+		this.fixUpTasks = fixUpTasks;
+	}
 
 }
