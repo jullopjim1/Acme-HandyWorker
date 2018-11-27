46fcad5788617a72c072179186ef37b75738df9b
adacieizq <32980210+adacieizq@users.noreply.github.com>
2018-11-27 19:46:23 +0100

    test + regex application + restriccion status de aplication

--- Acme-HandyWorker/src/main/java/domain/CreditCard.java
+++ Acme-HandyWorker/src/main/java/domain/CreditCard.java
@@ -1,13 +1,8 @@
 package domain;
 
-import java.util.Collection;
-
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
--- Acme-HandyWorker/src/test/java/service/ApplicationServiceTest.java
+++ Acme-HandyWorker/src/test/java/service/ApplicationServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
 
 import services.ApplicationService;
+import services.CreditCardService;
 import services.FixUpTaskService;
 import services.HandyWorkerService;
 import utilities.AbstractTest;
@@ -32,25 +33,59 @@ public class ApplicationServiceTest extends AbstractTest {
 
 	@Autowired
 	private FixUpTaskService		fixUpTaskService;
+	
+	@Autowired
+	private CreditCardService		creditCardService;
+	
 	//Test-------------------------------------------------------------
 
 	@Test
 	public void test() {
+		//LOGIN COMO HANDYWORKER
+		authenticate("handyworker1");
+		
+		//CREO APPLICATION Y SETEO ATRIBUTOS
 		final Application application = applicationService.create();
 		application.setStatus("PENDING");
 		application.setPrice(23);
 		application.setComments("comments");
 		final int handyWorkerId = this.getEntityId("handyWorker1");
 		application.setHandyWorker(handyWorkerService.findOne(handyWorkerId));
+		
+		//FIXUPTASK DE CUSTOMER1
 		final int fixUpTaskId = this.getEntityId("fixuptask1");
 		application.setFixUpTask(fixUpTaskService.findOne(fixUpTaskId));
+		final int creditCardId = this.getEntityId("creditcard1");
+		application.setCreditCard(creditCardService.findOne(creditCardId));
+		
+		//GUARDO APPLICATION
 		Application saved = applicationService.save(application);
+		
+		//CHECK FINDALL
 		Assert.isTrue(applicationService.findAll().contains(saved));
+		
+		//CHECK FINDONE
 		Assert.isTrue(applicationService.findOne(saved.getId()) == saved);
+		
+		//EDITO APPLICATION (TAMBIEN CHEKEO RESTRICCION STATUS Y CREDITCARD)
 		saved.setPrice(28);
+		saved.setStatus("ACCEPTED");
+		
+		//LOGIN COMO CUSTOMER
+		unauthenticate();
+		authenticate("customer1");
+		
+		//GUARDO APPLICATION EDITADA
 		Application saved2 = applicationService.save(saved);
 		Assert.isTrue(applicationService.findOne(saved2.getId()).getPrice()==28);
+		
+		//LOGIN COMO ADMIN
+		unauthenticate();
+		authenticate("admin");
+		
+		//BORRO APPLICATION
 		applicationService.delete(saved);
 		Assert.isTrue(!applicationService.findAll().contains(saved));
+		unauthenticate();
 	}
 }
