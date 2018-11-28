b34bff427399821fff67fda13b899ff10433728b
Julian Lopez <julianlj94@hotmail.com>
2018-11-28 12:01:27 +0100

    Las queries estadisticas se han repartido

--- Acme-HandyWorker/src/main/java/repositories/CustomerRepository.java
+++ Acme-HandyWorker/src/main/java/repositories/CustomerRepository.java
@@ -1,6 +1,8 @@
+
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.Customer;
@@ -8,4 +10,7 @@ import domain.Customer;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+	//Queries Level C---------------------------------------------------------
+	@Query("select h.name from HandyWorker h where ((select count(a) from Application a where a.status = 'ACCEPTED') > (select avg(hh.applications.size)*1.1 from HandyWorker hh join hh.applications aa where aa.status = 'ACCEPTED')) order by h.applications.size desc")
+	public Object[] queryC10();
 }
