f3292f6f1a37b756c56491baabfae502be6309a2
danielhingar <danielhingar3397@gmail.com>
2018-12-28 19:25:26 +0100

    Update TutorialHandyWorkerController.java

--- Acme-HandyWorker/src/main/java/controllers/HandyWorker/TutorialHandyWorkerController.java
+++ Acme-HandyWorker/src/main/java/controllers/HandyWorker/TutorialHandyWorkerController.java
@@ -3,10 +3,15 @@ package controllers.HandyWorker;
 
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import security.LoginService;
@@ -68,58 +73,39 @@ public class TutorialHandyWorkerController extends AbstractController {
 
 		return result;
 	}
-	/*
-	 * @RequestMapping(value = "/edit", method = RequestMethod.GET)
-	 * public ModelAndView edit(@RequestParam final int sectionId) {
-	 * ModelAndView result;
-	 * Section section;
-	 * 
-	 * section = this.sectionService.findOne(sectionId);
-	 * Assert.notNull(section);
-	 * result = this.createEditModelAndView(section);
-	 * 
-	 * return result;
-	 * }
-	 * 
-	 * //Save
-	 * 
-	 * @RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
-	 * public ModelAndView save(@Valid final Section section, final BindingResult binding) {
-	 * 
-	 * ModelAndView result;
-	 * 
-	 * if (binding.hasErrors())
-	 * result = this.createEditModelAndView(section);
-	 * else
-	 * try {
-	 * this.sectionService.save(section);
-	 * result = new ModelAndView("redirect:/section/list.do?tutorialId=" + section.getTutorial().getId());
-	 * } catch (final Throwable oops) {
-	 * if (oops.getMessage().equals("positionerror"))
-	 * result = this.createEditModelAndView(section, "position.error");
-	 * else
-	 * result = this.createEditModelAndView(section, "section.commit.error");
-	 * 
-	 * }
-	 * return result;
-	 * }
-	 * 
-	 * //Delete
-	 * 
-	 * @RequestMapping(value = "/edit", method = RequestMethod.POST, params = "delete")
-	 * public ModelAndView delete(@Valid final Section section, final BindingResult binding) {
-	 * 
-	 * ModelAndView result;
-	 * 
-	 * try {
-	 * this.sectionService.delete(section);
-	 * result = new ModelAndView("redirect:/section/list.do?tutorialId=" + section.getTutorial().getId());
-	 * } catch (final Throwable oops) {
-	 * result = this.createEditModelAndView(section, "section.commit.error");
-	 * }
-	 * return result;
-	 * }
-	 */
+
+	//Edit
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam final int tutorialId) {
+		ModelAndView result;
+		Tutorial tutorial;
+
+		tutorial = this.tutorialService.findOne(tutorialId);
+		Assert.notNull(tutorial);
+		result = this.createEditModelAndView(tutorial);
+
+		return result;
+	}
+
+	//Save
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid final Tutorial tutorial, final BindingResult binding) {
+
+		ModelAndView result;
+
+		if (binding.hasErrors())
+			result = this.createEditModelAndView(tutorial);
+		else
+			try {
+				this.tutorialService.save(tutorial);
+				result = new ModelAndView("redirect:list.do");
+			} catch (final Throwable oops) {
+				result = this.createEditModelAndView(tutorial, "tutorial.commit.error");
+
+			}
+		return result;
+	}
+
 	protected ModelAndView createEditModelAndView(final Tutorial tutorial) {
 		ModelAndView result;
 
