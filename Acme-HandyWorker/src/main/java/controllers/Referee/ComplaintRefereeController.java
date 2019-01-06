c09342b942ff40432a54ac174daf3fe0b7e16017
Jesusmtorres97 <jesus.torres.mateo@gmail.com>
2019-01-06 23:03:41 +0100

    Create ComplaintRefereeController

---
+++ Acme-HandyWorker/src/main/java/controllers/Referee/ComplaintRefereeController.java
@@ -0,0 +1,57 @@
+
+package controllers.Referee;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import security.LoginService;
+import services.ComplaintService;
+import services.RefereeService;
+import controllers.AbstractController;
+import domain.Complaint;
+import domain.Referee;
+
+@Controller
+@RequestMapping("/complaint/referee")
+public class ComplaintRefereeController extends AbstractController {
+
+	//Service---------------------------------------------------------
+
+	@Autowired
+	private ComplaintService	complaintService;
+
+	@Autowired
+	private RefereeService		refereeService;
+
+
+	//Constructor-----------------------------------------------------
+
+	public ComplaintRefereeController() {
+		super();
+	}
+
+	//List------------------------------------------------------------
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		final ModelAndView result;
+		final Collection<Complaint> complaints;
+
+		final Referee r = this.refereeService.findByUseraccount(LoginService.getPrincipal());
+		final int refereeId = r.getId();
+		//		complaints = this.complaintService.;
+
+		result = new ModelAndView("complaint/list");
+		//		result.addObject("complaints", complaints);
+		result.addObject("refereeId", refereeId);
+		result.addObject("requestURI", "/list.do?refereeId=?" + refereeId);
+
+		return result;
+
+	}
+}
