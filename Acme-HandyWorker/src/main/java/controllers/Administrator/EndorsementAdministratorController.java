614fe5b7b388b6e6b441ba1416fb2bd168e09944
Ferkaj <f_javi_fm@hotmail.com>
2019-01-07 17:07:55 +0100

    Update EndorsementAdministratorController.java

--- Acme-HandyWorker/src/main/java/controllers/Administrator/EndorsementAdministratorController.java
+++ Acme-HandyWorker/src/main/java/controllers/Administrator/EndorsementAdministratorController.java
@@ -1,28 +1,22 @@
 
 package controllers.Administrator;
 
-import java.util.ArrayList;
 import java.util.Collection;
-
-import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import security.LoginService;
 import services.EndorsementService;
 import services.EndorserService;
 import controllers.AbstractController;
-import domain.Endorsement;
 import domain.Endorser;
 
 @Controller
-@RequestMapping("/endorsement")
+@RequestMapping("/endorsement/administrator")
 public class EndorsementAdministratorController extends AbstractController {
 
 	@Autowired
@@ -35,17 +29,9 @@ public class EndorsementAdministratorController extends AbstractController {
 	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	public ModelAndView list() {
 		final ModelAndView result;
-		Collection<Endorsement> endorsements = new ArrayList<>();
-		result = new ModelAndView("endorsement/list");
+
 		try {
-			final Endorser endorser = this.endorserService.findEndorserByUseraccount(LoginService.getPrincipal());
-			endorsements = this.endorsementService.findByEndorser(endorser);
-			final Collection<Endorsement> endorsementsEndorsee = this.endorsementService.findByEndorsee(endorser);
-			final Double score = endorser.getScore();
-			result.addObject("score", score);
-			result.addObject("endorsements", endorsements);
-			result.addObject("endorsementsEndorsee", endorsementsEndorsee);
-			result.addObject("requestURI", "endorsements/list.do");
+			result = this.listCalculateModelAndView();
 		} catch (final Exception e) {
 			result.setViewName("welcome/index.do");
 			result.addObject("message", "message.commit.error");
@@ -54,105 +40,36 @@ public class EndorsementAdministratorController extends AbstractController {
 		return result;
 	}
 
-	//Create
-	@RequestMapping(value = "/create", method = RequestMethod.GET)
-	public ModelAndView create() {
-		ModelAndView modelAndView;
-		try {
-			final Endorsement endorsement = this.endorsementService.create();
-
-			modelAndView = this.createEditModelAndView(endorsement);
-
-		} catch (final Exception e) {
-			modelAndView = new ModelAndView("welcome/index.do");
-			modelAndView.addObject("message", "message.commit.error");
-		}
-
-		return modelAndView;
-	}
-
-	@RequestMapping(value = "/show", method = RequestMethod.GET)
-	public ModelAndView show(@RequestParam final int endorsementId) {
-		ModelAndView modelAndView;
-		try {
-
-			final Endorsement endorsement = this.endorsementService.findOne(endorsementId);
-			this.endorsementService.checkPrincipal(endorsement);
-			modelAndView = this.createEditModelAndView(endorsement);
-			modelAndView.addObject("isRead", true);
-		} catch (final Exception e) {
-			modelAndView = new ModelAndView("redirect:/endorsement/list.do");
-		}
-
-		return modelAndView;
-	}
-
-	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	@RequestMapping(value = "/calculateScore", method = RequestMethod.GET)
 	public ModelAndView edit(@RequestParam final int endorsementId) {
 		ModelAndView modelAndView;
 		try {
-
-			final Endorsement endorsement = this.endorsementService.findOne(endorsementId);
-			this.endorsementService.checkPrincipal(endorsement);
-			modelAndView = this.createEditModelAndView(endorsement);
+			// llamar a calcular score
+			modelAndView = this.listCalculateModelAndView("endorsement.commit.ok");
 		} catch (final Exception e) {
-			modelAndView = new ModelAndView("redirect:/endorsement/list.do");
+			modelAndView = this.listCalculateModelAndView("message.commit.error");
 		}
 
 		return modelAndView;
 	}
 
-	//Save
-	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
-	public ModelAndView save(@Valid final Endorsement endorsement, final BindingResult binding) {
-
+	protected ModelAndView listCalculateModelAndView() {
 		ModelAndView result;
 
-		if (binding.hasErrors())
-			result = this.createEditModelAndView(endorsement);
-		else
-			try {
-				this.endorsementService.save(endorsement);
-				result = new ModelAndView("redirect:/endorsement/list.do");
-			} catch (final Throwable oops) {
-				result = this.createEditModelAndView(endorsement, "message.commit.error");
-			}
-		return result;
-	}
-
-	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "delete")
-	public ModelAndView save(final Endorsement endorsement) {
-
-		ModelAndView result;
-		try {
-			this.endorsementService.delete(endorsement);
-			result = new ModelAndView("redirect:/endorsement/list.do");
-		} catch (final Throwable oops) {
-			result = this.createEditModelAndView(endorsement, "message.commit.error");
-		}
-		return result;
-	}
-	//CreateModelAndView
-
-	protected ModelAndView createEditModelAndView(final Endorsement endorsement) {
-		ModelAndView result;
-
-		result = this.createEditModelAndView(endorsement, null);
+		result = this.listCalculateModelAndView(null);
 
 		return result;
 
 	}
 
-	protected ModelAndView createEditModelAndView(final Endorsement endorsement, final String message) {
+	protected ModelAndView listCalculateModelAndView(final String message) {
 		ModelAndView result;
 
-		final Collection<Endorser> endorsees = this.endorserService.findEndorsees();
+		final Collection<Endorser> endorser = this.endorserService.findAll();
 
-		result = new ModelAndView("endorsement/edit");
-		result.addObject("endorsement", endorsement);
-		result.addObject("endorsees", endorsees);
+		result = new ModelAndView("endorsement/administrator/listEndorser");
+		result.addObject("endorser", endorser);
 		result.addObject("message", message);
-		result.addObject("isRead", false);
 		return result;
 	}
 
